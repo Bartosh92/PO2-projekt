9497cd1c1e11e9e38367246a736879bbab3d4789
Piotr Woś <piotr.wosss@gmail.com>
2024-12-31 16:21:46 +0100

    Dodanie miejsca na login

--- src/main/java/org/example/Login.java
+++ src/main/java/org/example/Login.java
@@ -2,17 +2,23 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class Login {
     JFrame loginFrame = new JFrame();
     JPanel loginPanel = new JPanel();
     JPanel LoginLabelPanel = new JPanel();
-    JLabel LoginLabel = new JLabel("Login");
+    JLabel LoginLabel = new JLabel("Log in");
+    JTextField login = new JTextField();
 
     public Login() {
         Initalize_Window();
+        CreateLoginChatArea();
         CreateLoginPanel();
-        setLoginPanel();
+
     }
 
     public void Initalize_Window()
@@ -31,20 +37,60 @@ public class Login {
         this.loginPanel.setBackground(Color.WHITE);
     }
 
+    //Tworzy glowny panel i etykiete
     public void CreateLoginPanel()
     {
-        this.LoginLabelPanel.setLayout(null);
-        this.LoginLabel.setBounds(50, 50, 100, 30);
-        this.LoginLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
+        this.LoginLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        this.LoginLabel.setFont(this.LoginLabel.getFont().deriveFont(Font.BOLD, 20));
         this.LoginLabel.setForeground(Color.BLACK);
         this.LoginLabelPanel.add(this.LoginLabel);
+        this.loginFrame.add(this.LoginLabelPanel, BorderLayout.NORTH);
     }
 
-    public void setLoginPanel(){
-        this.loginFrame.add(this.LoginLabelPanel);
+
+
+    public void CreateLoginChatArea(){
+
+        this.login.setPreferredSize(new Dimension(550, 30));
+        this.login.setFont(new Font("Tahoma", Font.PLAIN, 12));
+        this.login.setForeground(Color.BLACK);
+        this.login.setBackground(Color.WHITE);
+        this.loginPanel.add(this.login, BorderLayout.CENTER);
+
+
+        //Obsluga klawisza enter
+        this.login.addKeyListener(new KeyListener() {
+            public void keyTyped(KeyEvent e) {}
+            public void keyPressed(KeyEvent e) {
+                switch(e.getKeyCode()) {
+                    case KeyEvent.VK_ENTER:
+                        login.setText(" ");
+                }
+            }
+
+            public void keyReleased(KeyEvent e) {}
+
+        });
+
+        login.addFocusListener(new FocusListener() {
+            public void focusGained(FocusEvent e)
+            {
+                login.setText("Login");
+            }
+
+            public void focusLost(FocusEvent e){}
+        });
+
+
+
     }
 
-    public void C
+    public static void main(String[] args) {
+        Login login = new Login();
+
+
+    }
+
 
 
 }
