eabe0d3b8463b5f0c4c0d517a2ca6a743e7f3003
Filipeczek 01 <filipwieckowicz@gmail.com>
2025-01-14 18:30:40 +0100

    przycisk czyszczący chat

--- src/main/java/org/example/Chat.java
+++ src/main/java/org/example/Chat.java
@@ -3,6 +3,7 @@ package org.example;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
+import javax.swing.JButton;
 
 //Klasa służąca do stworzenia chatboxa
 
@@ -12,6 +13,7 @@ public class Chat {
     private JPanel chat_panel = new JPanel(); //Główny panel chatu, w którym znajdują się pozostałe obiekty
     private JPanel message_panel = new JPanel(); // Panel w którym znajduje się obszar wyświetlanych wiadomości
     private Client client;
+    private JButton clearButton = new JButton("Wyczyść czat");
 
     public Chat(Client client){
         this.client = client;
@@ -19,6 +21,7 @@ public class Chat {
         createMessagePanel();
         createChatPanel();
         createMessageArea();
+        addClearButton(); // Dodaj przycisk
         renderPanel();
     }
 
@@ -67,6 +70,12 @@ public class Chat {
         this.chat_panel.setLayout(new BorderLayout()); //ustawia layout
         this.chat_panel.add(this.message_panel, BorderLayout.NORTH);
         this.chat_panel.add(new JScrollPane(this.chatArea), BorderLayout.CENTER);
+    }
+
+    private void addClearButton() {
+        clearButton.setPreferredSize(new Dimension(150, 30));
+        clearButton.addActionListener(e -> clearChat()); // Powiązanie z akcją
+        chat_panel.add(clearButton, BorderLayout.SOUTH); // Dodanie do panelu
     }
 
     public void clearChat() {
